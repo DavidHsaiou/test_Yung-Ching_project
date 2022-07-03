cf0c2016e1fb88c56306bb16675d8aa288a32c6e
davidhsaiou <t437047t@hotmail.com>
2022-07-03 16:03:05 +0800

    add item model using entity, and simple views by ef tools 2

---
+++ test_Yung-Ching_project/Controllers/ItemsBagEntity.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using test_Yung_Ching_project.Data;
+using test_Yung_Ching_project.Models;
+
+namespace test_Yung_Ching_project.Controllers
+{
+    public class ItemsBagEntity : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsBagEntity(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ItemsBagEntity
+        public async Task<IActionResult> Index()
+        {
+              return _context.ItemModel != null ? 
+                          View(await _context.ItemModel.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.ItemModel'  is null.");
+        }
+
+        // GET: ItemsBagEntity/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ItemModel == null)
+            {
+                return NotFound();
+            }
+
+            var itemModel = await _context.ItemModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (itemModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemModel);
+        }
+
+        // GET: ItemsBagEntity/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemsBagEntity/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Memo,CreateTime")] ItemModel itemModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(itemModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(itemModel);
+        }
+
+        // GET: ItemsBagEntity/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ItemModel == null)
+            {
+                return NotFound();
+            }
+
+            var itemModel = await _context.ItemModel.FindAsync(id);
+            if (itemModel == null)
+            {
+                return NotFound();
+            }
+            return View(itemModel);
+        }
+
+        // POST: ItemsBagEntity/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Memo,CreateTime")] ItemModel itemModel)
+        {
+            if (id != itemModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(itemModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemModelExists(itemModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(itemModel);
+        }
+
+        // GET: ItemsBagEntity/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ItemModel == null)
+            {
+                return NotFound();
+            }
+
+            var itemModel = await _context.ItemModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (itemModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemModel);
+        }
+
+        // POST: ItemsBagEntity/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ItemModel == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.ItemModel'  is null.");
+            }
+            var itemModel = await _context.ItemModel.FindAsync(id);
+            if (itemModel != null)
+            {
+                _context.ItemModel.Remove(itemModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemModelExists(int id)
+        {
+          return (_context.ItemModel?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ test_Yung-Ching_project/Data/Migrations/20220703075844_ItemBagEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace test_Yung_Ching_project.Data.Migrations
+{
+    public partial class ItemBagEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ErrorViewModel",
+                columns: table => new
+                {
+                    RequestId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Memo = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ErrorViewModel");
+
+            migrationBuilder.DropTable(
+                name: "ItemModel");
+        }
+    }
+}
