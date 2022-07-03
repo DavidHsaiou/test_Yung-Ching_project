2f13679f4f202dfca1f7b667a8d5565e71d082fc
davidhsaiou <t437047t@hotmail.com>
2022-07-03 20:14:24 +0800

    add test code for item service

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using test_Yung_Ching_project.Data;
+using test_Yung_Ching_project.Exceptions;
+using test_Yung_Ching_project.Models;
 using test_Yung_Ching_project.Repositories;
 using test_Yung_Ching_project.Services;
 
@@ -5,16 +9,82 @@ namespace UnitTest;
 
 public class Tests
 {
+    private IItemService _itemService;
+    
     [SetUp]
     public void Setup()
     {
-        // _itemRepo = new ItemRepo();
-        // _itemService = new ItemService();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var dbSet = new ApplicationDbContext(options);
+        var itemRepo = new ItemRepo(dbSet);
+        _itemService = new ItemService(itemRepo);
+        
+        // test data
+        _itemService.Create(new ItemModel()
+        {
+            Id = 1,
+            Name = "a",
+            CreateTime = DateTime.Now,
+            Memo = "test data 1"
+        });
+        
+        _itemService.Create(new ItemModel()
+        {
+            Id = 2,
+            Name = "b",
+            CreateTime = DateTime.Now,
+            Memo = "test data 2"
+        });
     }
 
     [Test]
-    public void Test1()
+    public async Task Test_New_Data()
     {
-        Assert.Pass();
+        var newData = new ItemModel()
+        {
+            Id = 999,
+            CreateTime = DateTime.Now,
+            Memo = "Hello world",
+            Name = "abc"
+        };
+        await _itemService.Create(newData);
+
+        var result = await _itemService.GetDetail(999);
+        Assert.AreEqual(result, newData);
+    }
+
+    [Test]
+    public async Task Test_ReadData()
+    {
+        var result = await _itemService.GetDetail(1);
+        Assert.AreEqual(result.Name, "a");
+    }
+
+    [Test]
+    public async Task Tast_UpdateData()
+    {
+        var getData = await _itemService.GetDetail(1);
+        getData.Name = "new data";
+        await _itemService.Edit(getData);
+
+        getData = await _itemService.GetDetail(1);
+        Assert.AreEqual(getData.Name, "new data");
+    }
+
+    [Test]
+    public async Task Test_DeleteData()
+    {
+        await _itemService.Remove(1);
+        try
+        {
+            var getData = await _itemService.GetDetail(1);
+        }
+        catch (ItemNotFoundException)
+        {
+            Assert.Pass();
+        }
+        Assert.Fail();
     }
 }
